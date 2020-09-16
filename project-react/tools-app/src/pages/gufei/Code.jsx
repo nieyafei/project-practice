80e87cf2566e0f2c337cd4d133fdf657fc6aa9a3
nyf <327675933@qq.com>
2020-09-16 17:07:21 +0800

    update 更新验证码获取

--- project-react/tools-app/src/pages/gufei/Code.jsx
+++ project-react/tools-app/src/pages/gufei/Code.jsx
@@ -1,16 +1,21 @@
 import React, {useState} from 'react';
-import { Link } from "react-router-dom";
-import { apiResp } from '../../global/api'
+import { api } from '../../global/api';
 import { PageCardReset } from "../../component/Common";
-import { Row, Input, Col, Button, Card, Tag } from 'antd';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { Row, Input, Col, Button, Card, Tag, message } from 'antd';
 const { Search } = Input;
-
+const dataConfig = [
+  {title: '测试服验证码' , url: 'http://dev.jiaxinggufei.com/api/user/code/'},
+  {title: '正式服验证码' , url: 'http://jiaxinggufei.com/api/user/code/'},
+  {title: '测试服三大十招验证码' , url: 'https://sandashizhao.dev.jiaxinggufei.com/api/skill/wechat/code/'},
+  {title: '正式服三大十招验证码' , url: 'https://sandashizhao.dev.jiaxinggufei.com/api/skill/wechat/code/'},
+];
 export default ({match})=> {
   return (
     <PageCardReset className={"index-section ghost"} parent={this} title="获取验证码" pageconfig={{keystr: "pdf"}}>
       {/* 获取路径的验证码 */}
       <Row gutter={16}>
-        <CodeInput title={'测试服验证码'} url="http://dev.jiaxinggufei.com/api/user/code/" />
+        {dataConfig.map((d,index)=> <CodeInput {...d} key={index} />)}
       </Row>
     </PageCardReset>
   )
@@ -21,29 +26,23 @@ const CodeInput=(props)=>{
   const [code, setCode] = useState('000000');
   const {url, title} = props;
   const onSearch =async (value)=> {
-    let resp = await apiResp(url+value);
-    /* fetch(url+value, {method:'GET', mode: "cors", headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST',
-      'Access-Control-Allow-Headers': 'X-Requested-With',
-      'Access-Control-Allow-Credentials': true,
-      'Content-Type': 'application/json;charset=utf-8'
-    }}).then(res=> {
-      console.log(res)
-      return res.json();
-    }).then(json=> {
-      console.log(json);
-    }).catch(err=> {
-      console.log(err);
-    }) */
+    setLoading(true);
+    const {code, data} = await api('/api/global/resp-json', {url: url+value});
+    if(code !== -1){
+      const result = JSON.parse(data?.result || {});
+      setCode(result?.code || result);
+    }else{
+      message.error(data);
+    }
+    setLoading(false);
   }
   return (
-    <Col span={12}>
+    <Col span={12} style={{marginBottom: '16px'}}>
       <Card title={title}>
         <br />
         <h3>链接：<Tag>{url}手机号</Tag></h3>
         <Search placeholder="请输入手机号" loading={loading} enterButton onSearch={onSearch} style={{margin: '20px 0'}} />
-        验证码：<Tag color="green">{code}</Tag><Button type="primary" size="small">复制</Button>
+        验证码：<Tag color="green">{code}</Tag><CopyToClipboard text={code} onCopy={() => message.success('复制成功')}><Button type="primary" size="small">复制</Button></CopyToClipboard>
       </Card>
     </Col>
   )
